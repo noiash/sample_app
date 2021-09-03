619af4bb3d3b0f790ad0de152f039f454c43af73
FujiiNoi <noinoi51267@gmail.com>
2021-09-03 08:33:59 +0000

    [Update]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'aef46173325e3065be8c9f2c8adeccae136b1cbd4f066a95f16741bc58df53aa33c68340ea9c029b27f935f74ec0b517862240cefd96b2781d7fbfa8272c944a'
